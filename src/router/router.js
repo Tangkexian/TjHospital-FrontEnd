e8d66876a4e80b8b83d6849b3e1a90b1bb21f02f
1491456253 <1491456253@qq.com>
2024-05-17 15:43:22 +0800

    Finish AdminCheckin view

--- src/router/router.js
+++ src/router/router.js
@@ -12,12 +12,14 @@ import AskAI from '../views/Patient/AskAI.vue'
 
 // 管理员页面
 import AdminMedicine from '../views/Admin/AdminMedicine.vue'
-import AdminDoctor from '../views/Admin/AdminDoctor.vue'
-import AdminLayout from '../views/Admin/AdminLayout.vue'
+
 import AdminOrder from '../views/Admin/AdminOrder.vue'
+
 import AdminFeedback from '../views/Admin/AdminFeedback.vue'
 import AdminCheckin from '../views/Admin/AdminCheckin.vue'
 import MedicineCount from '../views/Admin/MedicineCount.vue'
+import AdminDoctor from '../views/Admin/AdminDoctor.vue'
+import AdminLayout from '../views/Admin/AdminLayout.vue'
 
 
 // import { use } from 'vue/types/umd'
@@ -101,30 +103,35 @@ const routes = [
     redirect: '/Admin/order',
     children: [
       {
-        path: 'order',
-        name: 'AdminOrder',
-        component: AdminOrder,
-      },
-      {
         path: 'medicine',
         name: 'AdminMedicine',
         component: AdminMedicine,
       },
+
+      {
+        path: 'order',
+        name: 'AdminOrder',
+        component: AdminOrder,
+      },
+
       {
         path: 'doctor',
         name: 'AdminDoctor',
         component: AdminDoctor,
       },
+
       {
         path: 'feedback',
         name: 'AdminFeedback',
         component: AdminFeedback,
       },
+
       {
         path: 'checkin',
         name: 'AdminCheckin',
         component: AdminCheckin,
       },
+
       {
         path: 'MedicineCount',
         name: 'MedicineCount',
