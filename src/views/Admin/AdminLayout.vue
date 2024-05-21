a48670de4bb17535ecc6db6322031e5c8fda3492
1491456253 <1491456253@qq.com>
2024-05-21 10:31:02 +0800

    管理员端界面基本完成

---
+++ src/views/Admin/AdminLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <SideBar :role="2"/>
+  <el-header>
+    <Header/>
+  </el-header>
+  <router-view/>
+</template>
+
+<script>
+import SideBar from '../../components/SideBar.vue';
+import Header from "../../components/Header.vue";
+
+export default {
+  components: {
+    SideBar,
+    Header,
+  },
+}
+</script>
+
+<style scoped>
+* {
+  font-family: AliRegular;
+  --va-font-family: AliRegular;
+  /* 应用字体 */
+}
+</style>
